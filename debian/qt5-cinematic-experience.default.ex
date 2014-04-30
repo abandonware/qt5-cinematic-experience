1d49f98a9272b6ac8334c37e68dee3f30d1ea904
Philippe Coval <rzr@gna.org>
2014-04-30 21:17:19 +0200

    packaging: debian
    Signed-off-by: Philippe Coval <rzr@gna.org>

---
+++ debian/qt5-cinematic-experience.default.ex
@@ -0,0 +1,10 @@
+# Defaults for qt5-cinematic-experience initscript
+# sourced by /etc/init.d/qt5-cinematic-experience
+# installed at /etc/default/qt5-cinematic-experience by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
